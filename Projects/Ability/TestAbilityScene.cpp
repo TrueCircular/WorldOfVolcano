df6b8ec3e2759a52ae27d90478d46138f81804db
TrueCircular <orijin0322@gmail.com>
2024-03-20 18:27:10 +0900

    dtd

---
+++ Projects/Ability/TestAbilityScene.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "TestAbilityScene.h"
+
+void TestAbilityScene::Init()
+{
+
+
+}
+
+void TestAbilityScene::Start()
+{
+	Scene::Start();
+}
+
+void TestAbilityScene::FixedUpdate()
+{
+	Scene::FixedUpdate();
+}
+
+void TestAbilityScene::Update()
+{
+	Scene::Update();
+}
+
+void TestAbilityScene::LateUpdate()
+{
+	Scene::LateUpdate();
+}
